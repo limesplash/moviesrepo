6a7dd1687c0eaf9eb2c0e930cbe1673b8faf07f6
Radoslav Minev <limesplash@abv.bg>
2017-09-15 17:37:24 +0300

    views hierarchy, binding and showing of mock data

---
+++ app/src/main/java/org/limesplasg/rminev/moviesapp/binding/BindingAdapters.java
@@ -0,0 +1,8 @@
+package org.limesplasg.rminev.moviesapp.binding;
+
+/**
+ * Created by Radoslav Minev on 15.9.2017 г..
+ */
+
+public class BindingAdapters {
+}
---
+++ app/src/main/java/org/limesplasg/rminev/moviesapp/fragment/CategoriesFragment.java
@@ -0,0 +1,8 @@
+package org.limesplasg.rminev.moviesapp.fragment;
+
+/**
+ * Created by Radoslav Minev on 15.9.2017 г..
+ */
+
+public class CategoriesFragment {
+}
